ead34d325a7013369b6858989ad906f840cc46b4
Lunic Dumitru <90331529+ChefDimas@users.noreply.github.com>
2023-06-06 19:46:25 +0200

    Commit

--- project_name/project_name/spiders/spider_name.py
+++ project_name/project_name/spiders/spider_name.py
@@ -3,8 +3,9 @@ import scrapy
 
 class SpiderNameSpider(scrapy.Spider):
     name = "spider_name"
-    allowed_domains = ["www.example.com"]
-    start_urls = ["https://www.example.com"]
+    allowed_domains = ["books.toscrape.com"]
+    start_urls = ["https://books.toscrape.com/"]
 
     def parse(self, response):
-        pass
+        alert = response.xpath('//*[@id="default"]/div/div/div/div/div[1]/h1').get()
+        yield {'alert': alert}
